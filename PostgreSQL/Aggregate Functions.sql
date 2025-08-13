a0837a3dfb7c9d3d851fbcc8e9e4f9560ce32846
Arman khan <armankhan271609@gmail.com>
2025-08-14 01:09:34 +0530

    Add files via upload

---
+++ PostgreSQL/Aggregate Functions.sql
@@ -0,0 +1,44 @@
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price NUMERIC(10, 2),
+    quantity INT,
+    added_date DATE,
+    discount_rate NUMERIC(5, 2)
+);
+INSERT INTO products (product_name, category, price, quantity, added_date, discount_rate) VALUES
+('Laptop', 'Electronics', 75000.50, 10, '2024-01-15', 10.00),
+('Smartphone', 'Electronics', 45000.99, 25, '2024-02-20', 5.00),
+('Headphones', 'Accessories', 1500.75, 50, '2024-03-05', 15.00),
+('Office Chair', 'Furniture', 5500.00, 20, '2023-12-01', 20.00),
+('Desk', 'Furniture', 8000.00, 15, '2023-11-20', 12.00),
+('Monitor', 'Electronics', 12000.00, 8, '2024-01-10', 8.00),
+('Printer', 'Electronics', 5900.50, 5, '2024-02-01', 7.50),
+('Mouse', 'Accessories', 750.00, 40, '2024-03-18', 10.00),
+('Keyboard', 'Accessories', 1250.00, 35, '2024-03-28', 10.00),
+('Tablet', 'Electronics', 30000.00, 12, '2024-02-28', 5.00);
+
+SELECT * FROM products;
+
+-- 1. Find total quantity of 'Electronics' AND price > 20000 -----------
+
+SELECT SUM(quantity) AS quantity_of_elec
+FROM products
+WHERE category='Electronics' AND price > 20000;
+
+-- 2. Count() with Condition --------------------------------------------
+
+SELECT COUNT(*) AS total_products
+FROM products
+WHERE product_name LIKE '%phone%';
+
+-- 3. Average() ---------------------------------------------------------
+SELECT AVG(price) AS average_price
+FROM products
+WHERE category='Accessories' OR added_date > '2024-02-01';
+
+-- 4. MIN(), MAX() ------------------------------------------------------
+SELECT MAX(price) AS max_price,
+	MIN(price) AS min_price 
+FROM products;
